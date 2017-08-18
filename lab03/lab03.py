02cda237cbb31a8752bb54a65cbe6e6f2e310c00
Yiluo Li <yiluo_li@csil-24.cs.ucsb.edu>
2017-08-18 12:33:47 -0700

    finish lab03

--- lab03/lab03.py
+++ lab03/lab03.py
@@ -18,6 +18,7 @@ def test_perimRect_3():
 def test_perimRect_4():
     assert perimRect(0.4, 11.2) == pytest.approx(23.2)
 
+#rectangle area
 def areaRect(length, width):
     """
     Compute area for the rectangle
@@ -32,3 +33,32 @@ def test_areaRect_2():
 
 def test_areaRect_3():
     assert areaRect(3, 4.2) == pytest.approx(12.6)
+
+#is string
+def isString(obj):
+    return (type(obj) == str)
+
+def test_isString_1():
+    assert isString("asdf")
+
+def test_isString_2():
+    assert isString("sa7890")
+
+def test_isString_3():
+    assert not isString(56789)
+
+#is number
+def isNumber(obj):
+    return (type(obj) == int or type(obj) == float)
+
+def test_isNumber_1():
+    assert isNumber(1234)
+
+def test_isNumber_2():
+    assert isNumber(890.123)
+
+def test_isNumber_3():
+    assert not isNumber("asdf")
+
+def test_isNumber_4():
+    assert not isNumber([789, 67890, 123])
