9f92f5709c98bad0202741609ec0add68f26c8bb
Yiluo Li <yiluo_li@cstl-31.cs.ucsb.edu>
2017-08-25 16:58:49 -0700

    initiate lab05

---
+++ lab05/lab05.py
@@ -0,0 +1,53 @@
+# lab05.py Yiluo Li
+import turtle
+import math
+
+if __name__ == "__main__":
+    t = turtle.Turtle()
+
+def drawRectabgle(width, height, tilt, penColor, fillColor):
+    """
+    draw rectangle. given width, height, penColor, and fillColor
+    current location: lower left corner
+    bottom side tilted by and angle, tilt in degrees
+    relative to horizontal axis
+    """
+
+def drawTriangle(base, height, tilt, penColor, fillColor):
+    """
+    draw triangle. given base, height, penColor, and fillColor 
+    current location: lower left corner
+    bottom side tilted by and angle, tilt in degrees
+    relative to horizontal axis
+    """
+
+def drawTree(height, color):
+    """
+    this function draw a tree
+    """
+
+def testdrawTree():
+    t.up()
+    t.goto(0, -400)
+    t.down()
+    drawRectangle(200, 200, 0, "200", "")
+    drawTree(200, gree)
+
+def testRectangle():
+    drawRectangle(50, 100, 0, "red", "")
+
+    t.seth(0)
+    t.up()
+    t.forward(200)
+    t.down()
+
+    drawRectangle
+
+    t.seth(0)
+    t.up()
+    t.forward(200)
+    t.down()
+
+testRectangle()
+
+
